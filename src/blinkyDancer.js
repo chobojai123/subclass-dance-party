05aecb1971bc4a96469ba63b63026f27a2990f6a
chobojai123 <davidcheng1290@gmail.com>
2018-03-12 21:22:28 -0700

    Updated

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -2,9 +2,9 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-    this.$node.addClass( "blinky" );
-    this.setPosition(top, left);
-    // this.lineUp(left);
+  this.$node.addClass( "blinky" );
+  this.setPosition(top, left);
+  // this.lineUp(left);
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
@@ -28,7 +28,7 @@ makeBlinkyDancer.prototype.setPosition = function(top, left) {
 
 
 makeBlinkyDancer.prototype.lineUp = function(left) {
-    var styleSettings = {
+  var styleSettings = {
     left: 0
   };
   this.$node.css(styleSettings);
--- src/init.js
+++ src/init.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
       $('body').width() * Math.random(),
       Math.random() * 1000
     );
-    $('body').append(dancer.$node)
+    $('body').append(dancer.$node);
     window.dancers.push(dancer);
   });
 
@@ -26,35 +26,6 @@ $(document).ready(function() {
     for (var i=0; i< window.dancers.length; i++) {
       window.dancers[i].$node.css(styleSettings);
     }    
-
-// $('.blinky').lineUp();
-  //   window.dancers.forEach(function(dancer){
-  //     $('body').height() * 500, 
-  //     $('body').width() * 500
-  //     // dancer.lineUp();
-  // });
-});
+  });
 
 });
-
-
-
-
-
-
-
-
-
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
-
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
